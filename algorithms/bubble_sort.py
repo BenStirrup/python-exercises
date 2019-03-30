2e67223abfcd74de9c5db31cf43f2e3661fcb293
BenStirrup <benjamin.stirrup@gmail.com>
2019-03-30 16:52:11 +0100

    Remove trivial swap function

--- algorithms/bubble_sort.py
+++ algorithms/bubble_sort.py
@@ -1,6 +1,3 @@
-from algorithms.utils import swap
-
-
 def bubble_sort(items):
     """ 
     Assumptions: items is a list of integers
@@ -12,7 +9,7 @@ def bubble_sort(items):
         are_items_sorted = True
         for j in range(0, len(items) - i - 1):
             if items[j] > items[j + 1]:
-                items[j], items[j + 1] = swap(items[j], items[j + 1])
+                items[j], items[j + 1] = items[j+1], items[j]
                 are_items_sorted = False
         i += 1
     return items
--- algorithms/utils.py
+++
@@ -1,2 +0,0 @@
-def swap(item1, item2):
-    return item2, item1
